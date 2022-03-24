6ce40a355d4c0b024195be15b2bf6a11d648da06
roberto castro <robertocastro@robertos-MacBook-Pro.local>
2022-03-24 11:37:22 -0300

    GETA

---
+++ main.js
@@ -0,0 +1,9 @@
+function plus(){
+let result = Number(document.getElementById("result").innerText)
+document.getElementById("result").innerText = result + 1
+} 
+
+function minus(){
+    let result = Number(document.getElementById("result").innerText)
+    document.getElementById("result").innerText = result - 1
+    } 
